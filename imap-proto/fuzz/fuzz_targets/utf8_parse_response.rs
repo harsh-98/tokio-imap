9029ceac65e2a2367f597e1d9d090348803ae70d
harsh-98 <harshjniitr@gmail.com>
2025-06-11 11:17:41 +0530

    fix: imap-proto

--- imap-proto/fuzz/fuzz_targets/utf8_parse_response.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate imap_proto;
-
-// UTF-8
-fuzz_target!(|data: &[u8]| {
-    let _ = imap_proto::Response::from_bytes(data);
-});
--- imap-proto/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod builders;
-pub mod parser;
-pub mod types;
-
-pub use parser::ParseResult;
-pub use types::*;
